dcefaa66a2e10aeee0dfaddb1f026d330fcaf04c
THANAPHOL NANTAKASET(Sa-Tang) <thanaphol369@gmail.com>
2023-04-22 13:20:08 +0700

    add Pretty urls (mod_rewrite)

--- yii2-pms/common/config/main.php
+++ yii2-pms/common/config/main.php
@@ -11,6 +11,19 @@ return [
     ],
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
     'components' => [
+        'urlManager' => [
+            'class' => 'yii\web\UrlManager',
+            // Disable index.php
+            'showScriptName' => false,
+            // Disable r= routes
+            'enablePrettyUrl' => true,
+            'rules' => array(
+                   '<controller:\w+>/<id:\d+>' => '<controller>/view',
+                     '<controller:\w+>/<action:\w+>/<id:\d+>' => '<controller>/<action>',
+                     '<controller:\w+>/<action:\w+>' => '<controller>/<action>',
+                     'module/<module:\w+>/<controller:\w+>/<action:\w+>' => '<module>/<controller>/<action>',
+            ),
+     ],
         'cache' => [
             'class' => \yii\caching\FileCache::class,
         ],
