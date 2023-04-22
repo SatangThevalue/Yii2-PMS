e55cde807bf521689a25310535073734a4a8b143
THANAPHOL NANTAKASET(Sa-Tang) <thanaphol369@gmail.com>
2023-04-22 12:25:30 +0700

    modified:   .gitignore 	new file:   Add Hosts.bat 	new file:   Ex .env 	modified:   config/php/php.ini 	modified:   logs/apache2/error.log

---
+++ Add Hosts.bat
@@ -0,0 +1,3 @@
+echo 127.0.0.1 db.pms.local >> %WINDIR%\System32\Drivers\Etc\Hosts
+echo 127.0.0.1 admin.pms.local >> %WINDIR%\System32\Drivers\Etc\Hosts
+echo 127.0.0.1 pms.local >> %WINDIR%\System32\Drivers\Etc\Hosts
