596fa734047622f2a59f6c419ee0bce8a0946325
THANAPHOL NANTAKASET(Sa-Tang) <thanaphol369@gmail.com>
2023-04-26 14:44:09 +0700

    install kartik-v/yii2-widgets

--- yii2-pms/common/config/params.php
+++ yii2-pms/common/config/params.php
@@ -6,4 +6,8 @@ return [
     'senderName' => 'Example.com mailer',
     'user.passwordResetTokenExpire' => 3600,
     'user.passwordMinLength' => 8,
+    'bsVersion' => '5.x', // this will set globally `bsVersion` to Bootstrap 5.x for all Krajee Extensions
+    // other settings
+    // 'adminEmail' => 'admin@example.com'
+    'bsDependencyEnabled' => false, // this will not load Bootstrap CSS and JS for all Krajee extensions
 ];
